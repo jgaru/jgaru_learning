d9c77707c8de1e972b7455408298b71d5253a2bc
jgaru <jaleg87@gmail.com>
2023-03-01 08:51:45 -0800

    Testing git movement

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jgaru_learning
 Learning and developing
+
+Third computer with git hub lets see what we make of this.
--- git_test.py
+++ git_test.py
@@ -1,7 +1,13 @@
+from time import sleep
 a = "This"
 b = ' is'
 c= ' a'
 d = ' test.'
 print(a+b+c+d)
 
-print('this is a change from the sencond PC')
+print('this is a change from the second PC')
+
+abc= "This is a test"
+for i in abc:
+    print(i)
+    sleep(0.5)
