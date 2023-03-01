8b54e530c54f0cd7cfb181a8866e41b523f97d43
jgaru <jaleg87@gmail.com>
2023-03-01 10:15:59 -0800

    Practicing git commands.

--- git_test.py
+++ git_test.py
@@ -1,4 +1,5 @@
 from time import sleep
+import pyautogui as pg
 a = "This"
 b = ' is'
 c= ' a'
@@ -9,5 +10,14 @@ print('this is a change from the second PC')
 
 abc= "This is a test"
 for i in abc:
-    print(i)
-    sleep(0.5)
+    print(i,end=" ")
+    sleep(0.1)
+
+counter = 0
+while counter <=2:
+    pg.moveTo(500,500,0.5)
+    pg.moveTo(750,500,0.5)
+    pg.moveTo(750,750,0.5)
+    pg.moveTo(500,750,0.5)
+    counter +=1
+    print(counter)
