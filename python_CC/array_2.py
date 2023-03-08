1233e544e0d5bd15ab19158ecb3d0c736bbef8b8
jgaru <jaleg87@gmail.com>
2023-03-07 19:44:51 -0800

    Based on W3S course, work on inheritance and iterables

--- python_CC/array_2.py
+++ python_CC/array_2.py
@@ -10,11 +10,23 @@ x = Person("john", "Doe")
 x.printname()
 
 class Student(Person):
-    def __init__(self,fname,lname):
+    def __init__(self,fname,lname,year):
         Person.__init__(self,fname,lname)
+        self.graduationyear = year
+    
+    def printstudent(self):
+        print(self.firstname, self.lastname, self.graduationyear)
+    
+    def saludo(self):
+        print("Bienvenido,", self.firstname, self.lastname,"te graduaste en el anho", self.graduationyear)
 
-x = Student("Mike","Olsen")
-x2 = Student("Jesus","Garmendia")
+x = Student("Mike","Olsen", 2019)
+x2 = Student("Jesus","Garmendia", 2016)
+
 
 x.printname()
+x.printstudent()
 x2.printname()
+x2.printstudent()
+
+x2.saludo()
